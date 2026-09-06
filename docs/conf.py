9ee62c6cc391e7278f093ef4375958985be489dc
QTE Technologies <87856590+qtetech@users.noreply.github.com>
2026-09-06 14:24:24 +0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'QTE Technologies MRO Wiki'
+copyright = '2026, QTE Technologies'
+author = 'QTE Technologies'
+release = '1.0.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
